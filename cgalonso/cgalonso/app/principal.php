096854b9d071d58a1e74175fe38a47a202cfc3e4
cgalonso <c.alonso@itecriocuarto.org.ar>
2015-06-05 21:48:08 -0300

    5 del 6

--- cgalonso/cgalonso/app/principal.php
+++ cgalonso/cgalonso/app/principal.php
@@ -4,10 +4,20 @@ include '../encabezado.php';
 <html>
 <body>
 <div align="center"
-	STYLE="background-color: cyan; width: 80%; position: absolute; top: 20%; left: 10%;">
+	STYLE="background-color: brown; width: 80%; position: absolute; top: 20%; left: 10%;">
 <br>
-<a href="list_user.php">Listado de Usuarios</a> <br>
-<a href="list_user_type.php">Listado de Tipos de Usuarios</a><br>.</div>
+<!--  <a href="list_user.php">Listado de Usuarios</a> <br>
+<a href="list_user_type.php">Listado de Tipos de Usuarios</a><br>.-->
+
+
+<form action="list_user.php"><font color="White" size="7"> 
+	<input	type='submit' value='Listado de Usuarios' style='FONT-SIZE: 20px; color: white; background: url("../img/sub-button-tile.gif") left center no-repeat; padding-left: 20px;' />
+</font></form>
+<br>
+<form action="list_user_type.php"><font color="White" size="7">
+ <input	type='submit' value='Listado de Tipos de Usuarios' style='FONT-SIZE: 20px; color: white; background: url("../img/sub-button-tile.gif") left center ; padding-left: 20px;' />
+</font></form>
+</div>
 </body>
 
 </html>
