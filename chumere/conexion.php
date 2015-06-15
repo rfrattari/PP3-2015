94a96a9a6c7245d647a805293e161b8cc932867c
chumere <krisdaniel00@gmail.com>
2015-06-15 18:41:37 -0300

    Update conexion.php

--- chumere/conexion.php
+++ chumere/conexion.php
@@ -1,21 +1,30 @@
+//<?php
+//function conectarse() {
+//    if (!($link = mysql_connect("localhost", "data4_pp3", "data42015php"))) {
+//        echo "conexion exitosa";
+//        exit();
+//    }
+//    if (!mysql_select_db("data4_pp3_db", $link)) {
+//        echo "Error seleccionando la base de datos.";
+//        exit();
+//    }
+//    return $link;
+//}
+//?>
+
+
+
+
+
+
+
+
+
+
 <?php
 
-$link = mysql_connect("localhost", "data4_pp3", "data42015php") or die("no se encuentra el servidor");
+$link = mysql_connect("localhost", "root", "humere") or die("no se encuentra el servidor");
 $db = mysql_select_db("data4_pp3_db", $link) or die("error de conexion");
 
 ?>
 
-
-<!--//
-////function conectarse() 
-////    if (!($link = mysql_connect("localhost", "data4_pp3", "data42015php"))) {
-////        echo "conexion exitosa";
-////        exit();
-////    }
-////    if (!mysql_select_db("data4_pp3_db", $link)) {
-////        echo "Error seleccionando la base de datos.";
-////        exit();
-////    }
-////    return $link;
-////}
-////?>-->
