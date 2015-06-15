fd66aacaa960177a81d73c521576a6691b1189c7
chumere <krisdaniel00@gmail.com>
2015-06-15 18:22:33 -0300

    Update conexion.php

--- chumere/conexion.php
+++ chumere/conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-$link = mysql_connect("localhost", "root", "humere") or die("no se encuentra el servidor");
+$link = mysql_connect("localhost", "data_pp3", "data42015php") or die("no se encuentra el servidor");
 $db = mysql_select_db("data4_pp3_db", $link) or die("error de conexion");
 
 ?>
 
 
 <!--//
-////function conectarse() {
+////function conectarse() 
 ////    if (!($link = mysql_connect("localhost", "data4_pp3", "data42015php"))) {
 ////        echo "conexion exitosa";
 ////        exit();
