193036b079d8b6812fbd9c16fc462380d7f0eda0
chumere <krisdaniel00@gmail.com>
2015-06-15 21:27:07 -0300

    Create conexion.php

---
+++ chumere/conexion.php
@@ -0,0 +1,23 @@
+//<?php
+//function conectarse() {
+//    if (!($link = mysql_connect("localhost", "data4_pp3", "data42015php"))) {
+//        echo "conexion exitosa";
+//        exit();
+//    }
+//    if (!mysql_select_db("data4_pp3_db", $link)) {
+//        echo "Error seleccionando la base de datos.";
+//        exit();
+//    }
+//    return $link;
+//}
+//?>
+
+
+
+<?php
+
+$link = mysql_connect("localhost", "root", "humere") or die("no se encuentra el servioiiiiiiidor");
+$db = mysql_select_db("data4_pp3_db", $link) or die("error de conexion");
+
+?>
+
