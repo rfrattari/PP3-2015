4fc488add763226670c19d66c9e34f4b11e2e863
chumere <krisdaniel00@gmail.comgit config --global user.email krisdaniel00@gmail.comgit config --global user.email krisdaniel00@gmail.com>
2015-06-15 21:37:16 -0300

    cambio

--- chumere/conexion.php
+++ chumere/conexion.php
@@ -14,10 +14,13 @@
 
 
 
-<?php
 
-$link = mysql_connect("localhost", "root", "humere") or die("no se encuentra el servioiiiiiiidor");
+
+<?php 
+
+$link = mysql_connect("localhost", "root", "humere") or die("no se encuentra el servidor");
 $db = mysql_select_db("data4_pp3_db", $link) or die("error de conexion");
 
 ?>
+
 
